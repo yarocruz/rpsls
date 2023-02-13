48d8e6129c08fb3d276f04ee1ac844d01d3c0488
yarocruz <yarocruz@gmail.com>
2023-02-12 22:30:05 -0500

    add message to indicate user who won and what was selected

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -18,16 +18,17 @@
       </div>
     </div>
     <div class="game-board">
-      <h1 class="computer-guess">{{ computerGuess ? computerGuess : 'Make a Move' }}</h1>
+      <h1 class="computer-guess">{{ computerGuess? computerGuess: 'Make a Move' }}</h1>
+      <h2>{{ message? message: "" }}</h2>
       <ul class="emojis">
 
-        <li class="emoji" v-for="emoji in emojis" v-bind:key="emoji.name" @click="clickIt(emoji.symbol)">
+        <li class="emoji" v-for="emoji in emojis" v-bind:key="emoji.name" @click="clickIt(emoji.symbol, emoji.name)">
           {{ emoji.symbol }}
-          <span class="name">{{ emoji.name}}</span>
+          <span class="name">{{ emoji.name }}</span>
         </li>
       </ul>
     </div>
-   <h1>Rules</h1>
+    <h1>Rules</h1>
     <ul class="rules">
       <li>Scissors cuts Paper</li>
       <li>Paper covers Rock</li>
@@ -44,138 +45,153 @@
 </template>
 
 <script>
-  export default {
-    name: 'Game',
-    data () {
-      return {
-        emojis: [
-          {
-            name: 'Rock',
-            symbol: '👊'
-          },
-          {
-            name: 'Paper',
-            symbol: '✋'
-          },
-          {
-            name: 'Scissor',
-            symbol: '✌️'
-          },
-          {
-            name: 'Lizard',
-            symbol: '🦎'
-          },
-          {
-            name: 'Spock',
-            symbol: '🖖'
-          },
-        ],
-        computerGuess: '',
-        computerWins: 0,
-        userWins: 0,
-      }
-    },
-    methods: {
-      clickIt: function (selection) {
-        let computerGuess = this.emojis[Math.floor(Math.random() * this.emojis.length)]
-        this.computerGuess = computerGuess.symbol
-        console.log(selection)
-        if (
-            (selection === '👊' && this.computerGuess === '✌️') ||
-            (selection === '✌️' && this.computerGuess === '✋') ||
-            (selection === '✋' && this.computerGuess === '👊') ||
-            (selection === '👊' && this.computerGuess === '🦎') ||
-            (selection === '🦎' && this.computerGuess === '🖖') ||
-            (selection === '✌️' && this.computerGuess === '🦎') ||
-            (selection === '🦎' && this.computerGuess === '✋') ||
-            (selection === '✋' && this.computerGuess === '🖖') ||
-            (selection === '🖖' && this.computerGuess === '👊')
-          ) {
-          this.userWins += 1
-          console.log(selection, this.computerGuess)
-        } else if (selection === computerGuess.symbol) {
-          console.log('Tied')
-        } else {
-          this.computerWins += 1
-          console.log(selection, this.computerGuess)
-        }
-
-      },
+export default {
+  name: 'Game',
+  data() {
+    return {
+      emojis: [
+        {
+          name: 'Rock',
+          symbol: '👊'
+        },
+        {
+          name: 'Paper',
+          symbol: '✋'
+        },
+        {
+          name: 'Scissor',
+          symbol: '✌️'
+        },
+        {
+          name: 'Lizard',
+          symbol: '🦎'
+        },
+        {
+          name: 'Spock',
+          symbol: '🖖'
+        },
+      ],
+      computerGuess: '',
+      message: '',
+      computerWins: 0,
+      userWins: 0,
     }
+  },
+  methods: {
+    clickIt: function (selectionEmoji, selectionName) {
+      let computerGuess = this.emojis[Math.floor(Math.random() * this.emojis.length)]
+      this.computerGuess = computerGuess.symbol
+      console.log(selectionEmoji)
+      if (
+        (selectionEmoji === '👊' && this.computerGuess === '✌️') ||
+        (selectionEmoji === '✌️' && this.computerGuess === '✋') ||
+        (selectionEmoji === '✋' && this.computerGuess === '👊') ||
+        (selectionEmoji === '👊' && this.computerGuess === '🦎') ||
+        (selectionEmoji === '🦎' && this.computerGuess === '🖖') ||
+        (selectionEmoji === '✌️' && this.computerGuess === '🦎') ||
+        (selectionEmoji === '🦎' && this.computerGuess === '✋') ||
+        (selectionEmoji === '✋' && this.computerGuess === '🖖') ||
+        (selectionEmoji === '🖖' && this.computerGuess === '👊')
+      ) {
+        this.userWins += 1
+        console.log(selectionEmoji, this.computerGuess)
+        this.message = `You win! You selected ${selectionName} and the Computer chose ${computerGuess.name}`
+      } else if (selectionEmoji === computerGuess.symbol) {
+        console.log('Tied')
+        this.message = `It's a tie! You both selected ${selectionName} and the Computer chose ${computerGuess.name}`
+      } else {
+        this.computerWins += 1
+        console.log(selectionEmoji, this.computerGuess)
+        this.message = `You lose! You selected ${selectionName} and the Computer chose ${computerGuess.name}`
+      }
+
+    },
   }
+}
 </script>
 
 <style>
-  .container {
-    max-width: 700px;
-    margin: 0 auto;
-  }
-  .emojis {
-    display: flex;
-    justify-content: space-between;
-    list-style: none;
-    padding: 0;
-  }
-  .emojis li {
-    font-size: 80px;
-    transition: transform 0.5s ease-in-out;
-  }
-  .emojis li:hover {
-    cursor: pointer;
-    transform: scale(1.3);
-  }
-  .emoji {
-    display: flex;
-    flex-direction: column;
-  }
-  .name {
-    font-size: 12px;
-    font-weight: bold;
-  }
-  .computer-guess {
-    font-size: 80px;
-  }
-  .game-board {
+.container {
+  max-width: 700px;
+  margin: 0 auto;
+}
 
-  }
-  .rules {
-    list-style: none;
-    text-align: center;
-    padding: 0;
-  }
-  .rules li {
-    margin: 10px auto;
-  }
-  .score-box {
-    max-width: 200px;
-    margin: 25px auto;
-    border-radius: 7px;
-    background-color: darkslategrey;
-    color: white;
-  }
-  .inner-border {
-    display: flex;
-    justify-content: center;
-    border-radius: 2px;
-  }
-  .score-box--count {
-    border-radius: 5px;
-    font-size: 25px;
-    width: 50px;
-  }
-  .score-box--container h4 {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 10px;
-  }
-  .score-box--computer,
-  .score-box--user {
-    background-color: black;
-    color: white;
-    border: 1px solid darkred;
-  }
-  .score-box--user {
-    border: 1px solid darkblue;
-  }
+.emojis {
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+}
+
+.emojis li {
+  font-size: 80px;
+  transition: transform 0.5s ease-in-out;
+}
+
+.emojis li:hover {
+  cursor: pointer;
+  transform: scale(1.3);
+}
+
+.emoji {
+  display: flex;
+  flex-direction: column;
+}
+
+.name {
+  font-size: 12px;
+  font-weight: bold;
+}
+
+.computer-guess {
+  font-size: 80px;
+}
+
+.rules {
+  list-style: none;
+  text-align: center;
+  padding: 0;
+}
+
+.rules li {
+  margin: 10px auto;
+}
+
+.score-box {
+  max-width: 200px;
+  margin: 25px auto;
+  border-radius: 7px;
+  background-color: darkslategrey;
+  color: white;
+}
+
+.inner-border {
+  display: flex;
+  justify-content: center;
+  border-radius: 2px;
+}
+
+.score-box--count {
+  border-radius: 5px;
+  font-size: 25px;
+  width: 50px;
+}
+
+.score-box--container h4 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+}
+
+.score-box--computer,
+.score-box--user {
+  background-color: black;
+  color: white;
+  border: 1px solid darkred;
+}
+
+.score-box--user {
+  border: 1px solid darkblue;
+}
 </style>
